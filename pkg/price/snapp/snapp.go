e3ddc57715904f3f576d61293b5949410cb7d244
Parisa Mousavi <parisa.mousavi@divar.ir>
2025-03-03 16:08:52 +0330

    Refactor snapp price estimation logic with structured request/response handling

--- pkg/price/snapp/snapp.go
+++ pkg/price/snapp/snapp.go
@@ -1,47 +1,92 @@
 package snapp
 
 import (
+	"bytes"
 	"encoding/json"
 	"io"
+	"log"
 	"net/http"
 	"os"
-	"strings"
 
 	"github.com/joho/godotenv"
 )
 
-func GetPrice(originLat, originLong, destinationLat, destinationLong string) int {
-	err := godotenv.Load()
-	if err != nil {
-		return -1
+type snappPoint struct {
+	Lat string `json:"lat"`
+	Lng string `json:"lng"`
+}
+
+type snappRequest struct {
+	Points         []snappPoint `json:"points"`
+	VoucherCode    any          `json:"voucher_code"`
+	ServiceTypes   []int        `json:"service_types"`
+	Priceriderecom bool         `json:"priceriderecom"`
+	Tag            string       `json:"tag"`
+	HurryRaised    int          `json:"hurryRaised"`
+}
+
+type snappPrices struct {
+	Final int `json:"final"`
+}
+
+type snappData struct {
+	Prices []snappPrices `json:"prices"`
+}
+type snappResponse struct {
+	Data snappData `json:"data"`
+}
+
+func GetSnappPriceEstimation(originLat, originLong, destinationLat, destinationLong string) int {
+	data := snappRequest{
+		Points: []snappPoint{
+			{Lat: originLat, Lng: originLong},
+			{Lat: destinationLat, Lng: destinationLong},
+		},
+		VoucherCode:    nil,
+		ServiceTypes:   []int{1, 2, 24},
+		Priceriderecom: false,
+		Tag:            "0",
+		HurryRaised:    0,
 	}
 
-	client := &http.Client{}
-	var data = strings.NewReader(`{"points":[{"lat":"` + originLat + `","lng":"` + originLong + `"},{"lat":"` + destinationLat + `","lng":"` + destinationLong + `"},null],"voucher_code":null,"service_types":[1,2,24],"priceriderecom":false,"tag":"0","hurryRaised":0}`)
-	req, err := http.NewRequest("POST", "https://app.snapp.taxi/api/api-base/v2/passenger/newprice/s/6/0", data)
+	dataBytes, err := json.Marshal(data)
 	if err != nil {
-		return -1
+		log.Fatal(err)
+	}
+	body := bytes.NewReader(dataBytes)
+	req, err := http.NewRequest("POST", "https://app.snapp.taxi/api/api-base/v2/passenger/newprice/s/6/0", body)
+	if err != nil {
+		log.Fatal(err)
 	}
 	setHeader(req)
+
+	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return -1
+		log.Fatal(err)
 	}
 	defer resp.Body.Close()
+
 	bodyText, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return -1
+		log.Fatal(err)
 	}
 
-	var jsonData map[string]interface{}
+	var jsonData snappResponse
 	err = json.Unmarshal(bodyText, &jsonData)
 	if err != nil {
-		return -1
+		log.Fatal(err)
 	}
-	return int(jsonData["data"].(map[string]interface{})["prices"].([]interface{})[0].(map[string]interface{})["final"].(float64)) / 10
+
+	return jsonData.Data.Prices[0].Final / 10
 }
 
 func setHeader(req *http.Request) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Referer", "https://app.snapp.taxi/pre-ride?utm_source=landing&utm_medium=request-button&utm_campaign=taxi&_gl=1*6bvi14*_gcl_au*MTEzNjQxNTUwMy4xNzQwNTc4NzI0")
 	req.Header.Set("Origin", "https://app.snapp.taxi")
