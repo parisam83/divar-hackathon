b1b8df112911f2cc541a56dbe26cbd16013fc5e9
Divar <sanamousavie@gmail.com>
2025-03-03 12:15:58 +0330

    feat: config from communication safety?

--- handlers/oauth.go
+++ handlers/oauth.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/sessions"
@@ -35,7 +34,7 @@ func NewOAuthHandler(serv *services.OAuthService) *oAuthHandler {
 	}
 }
 
-var store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_SECRET")))
+var store = sessions.NewCookieStore([]byte("oauth-session-secret"))
 
 func (h *oAuthHandler) AddonOauth(w http.ResponseWriter, r *http.Request) {
 
@@ -46,7 +45,7 @@ func (h *oAuthHandler) AddonOauth(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "post_token and return_url are required", http.StatusBadRequest)
 		return
 	}
-	oauthSession, err := store.Get(r, os.Getenv("OAUTH_SESSION_KEY"))
+	oauthSession, err := store.Get(r, "oauth-session")
 	if err != nil {
 		http.Error(w, "Failed to get session: "+err.Error(), http.StatusInternalServerError)
 		return
@@ -94,11 +93,24 @@ func (h *oAuthHandler) OauthCallback(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	//check if state is the same as the one in the session
-	// oauthSession, err := store.Get(r, os.Getenv("OAUTH_SESSION"))
-	// if err != nil {
-	// 	http.Error(w, "Failed to get session: "+err.Error(), http.StatusInternalServerError)
-	// 	return
-	// }
+	oauthSession, err := store.Get(r, "oauth-session")
+	if err != nil {
+		http.Error(w, "Failed to get session: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	sessionState, ok := oauthSession.Values["state"].(string)
+	if !ok || sessionState != state {
+		http.Error(w, "Invalid state", http.StatusBadRequest)
+		return
+	}
+	// deleting state from session because we dont need it after oauth
+	delete(oauthSession.Values, "state")
+	err = oauthSession.Save(r, w)
+	if err != nil {
+		http.Error(w, "Failed to save session after deleting state: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
 
 	//sending code to get the token
 	token, err := h.oauthService.ExchangeToken(r.Context(), code)
@@ -110,5 +122,11 @@ func (h *oAuthHandler) OauthCallback(w http.ResponseWriter, r *http.Request) {
 	expires_in := token.Expiry
 	log.Println(accessToken)
 	log.Println(expires_in)
+
+	//create the oauth object with session key , tokens and post
+	sessionKey := uuid.New().String()
+	oauthSession.Values["sessionKey"] = sessionKey
+	oauthSession.Save(r, w)
+	//redirect to api for poi
 
 }
