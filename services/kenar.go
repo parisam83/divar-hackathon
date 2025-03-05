7824495ed85a7a72b134922139dc92df88f14456
Divar <sanamousavie@gmail.com>
2025-03-06 01:15:58 +0330

    add subtitle to widget

--- services/kenar.go
+++ services/kenar.go
@@ -46,40 +46,57 @@ type Widget struct {
 	DescriptionRow map[string]interface{} `json:"description_row,omitempty"`
 }
 
-type Payload struct {
-	Widgets []Widget `json:"widgets"`
+type Row struct {
+	Key  string
+	Data map[string]interface{}
 }
+
+// We implement json.Marshaler interface for custom marshaling
+func (r Row) MarshalJSON() ([]byte, error) {
+	return json.Marshal(map[string]map[string]interface{}{
+		r.Key: r.Data,
+	})
+}
+
+type Payload struct {
+	Widgets []Row `json:"widgets"`
+}
+
+// type Payload struct {
+// 	Widgets []Widget `json:"widgets"`
+// }
 
 func (s *KenarService) PostWidgets(postToken string) {
 	log.Println("Post widgets")
 	payload := Payload{
-		Widgets: []Widget{
-			{
-				TitleRow:       map[string]interface{}{"text": "Sample Title"},
-				SubtitleRow:    map[string]interface{}{"text": "Sample Subtitle"},
-				DescriptionRow: map[string]interface{}{"text": "Sample Description"},
-			},
+		Widgets: []Row{
+			{"title_row", map[string]interface{}{"text": "Sample Title"}},
+			{"subtitle_row", map[string]interface{}{"text": "Sample Subtitle"}},
+			{"description_row", map[string]interface{}{"text": "Sample Description", "has_divider": false, "expandable": false}},
 		},
 	}
+
 	jsonData, err := json.MarshalIndent(payload, "", "  ")
 	if err != nil {
 		fmt.Println("Error marshaling JSON:", err)
 		return
 	}
 	// change ittttttttt
-	url := "https://api.divar.ir/v2/open-platform/addons/post" + postToken
+	url := "https://api.divar.ir/v2/open-platform/addons/post/" + postToken
 	req, err := http.NewRequest(http.MethodPost, url, strings.NewReader(string(jsonData)))
 	if err != nil {
 		log.Println("Error creating request:", err)
 		return
 	}
-	req.Header.Set("Content-Type", "application/json")
+	// req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("X-Api-Key", s.apiKey)
 	req.Header.Set("x-access-token", "ory_at_oGndTdJE-Cfq8-fuAAmFuHI_itqopsk7Pr8zQiBkPEQ.2FxEb-7SlGNzhF5tnduTXgfUxqoFeOnNlEzwHnEbunw")
-
+	log.Println("=======================================")
+	fmt.Println(s.client)
 	res, err := s.client.Do(req)
 	log.Println(res.Status)
-	log.Println(res.Status)
+	log.Println(err)
+	log.Println("=======================================")
 	if err != nil {
 		log.Println("request failed: %w", err)
 	}
@@ -99,33 +116,34 @@ func (s *KenarService) PostWidgets(postToken string) {
 }
 
 func (k *KenarService) GetCoordinates(postToken string) {
-	req, err := k.doRequest(http.MethodGet, "/finder/post/"+postToken, nil)
-	if err != nil {
-		log.Println("Error creating request:", err)
-		return
-	}
-	resp, err := k.client.Do(req)
-	if err != nil {
-		log.Println("Error sending request" + err.Error())
-		return
-	}
-	defer resp.Body.Close()
-	log.Println("Response.staus code:", resp.StatusCode)
+	k.PostWidgets(postToken)
+
+	// req, err := k.doRequest(http.MethodGet, "/finder/post/"+postToken, nil)
+	// if err != nil {
+	// 	log.Println("Error creating request:", err)
+	// 	return
+	// }
+	// resp, err := k.client.Do(req)
+	// if err != nil {
+	// 	log.Println("Error sending request" + err.Error())
+	// 	return
+	// }
+	// defer resp.Body.Close()
+	// log.Println("Response.staus code:", resp.StatusCode)
 	// if resp.StatusCode != http.StatusOK {
 	// 	log.Println("Error response code:", resp.StatusCode)
 	// 	return
+	// // }
+	// body, _ := io.ReadAll(resp.Body)
+	// var jsonData map[string]interface{}
+	// err = json.Unmarshal(body, &jsonData)
+	// if err != nil {
+	// 	log.Println("Error parsing response:", err)
+	// 	return
 	// }
-	body, _ := io.ReadAll(resp.Body)
-	var jsonData map[string]interface{}
-	err = json.Unmarshal(body, &jsonData)
-	if err != nil {
-		log.Println("Error parsing response:", err)
-		return
-	}
-	k.PostWidgets(postToken)
 
-	fmt.Println(jsonData["data"].(map[string]interface{})["latitude"])
-	fmt.Println(jsonData["data"].(map[string]interface{})["longitude"])
+	// fmt.Println(jsonData["data"].(map[string]interface{})["latitude"])
+	// fmt.Println(jsonData["data"].(map[string]interface{})["longitude"])
 
 	log.Println("Request successful")
 }
