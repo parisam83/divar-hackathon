6b0986a644c4aeda6a83c3b8cc701f5738cf705b
Parisa Mousavi <parisa.mousavi@divar.ir>
2025-04-07 10:46:04 +0330

    fix: clean internal/handlers/page-handlers.go

--- internal/handlers/page-handlers.go
+++ internal/handlers/page-handlers.go
@@ -26,16 +26,21 @@ func NewPageHandler(
 		taxiService:  taxiService,
 	}
 }
+
 func (p *PageHandler) BuyerDashboardHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("internal/handlers/BuyerDashboardHandler called")
+
 	postToken := r.URL.Query().Get("post_token")
 	return_url := r.URL.Query().Get("return_url")
 
 	if postToken == "" || return_url == "" {
+		log.Printf("post_token and return_url are required")
 		http.Error(w, "post_token and return_url are required", http.StatusBadRequest)
 		return
 	}
 	userID, ok := r.Context().Value("user_id").(string)
 	if !ok {
+		log.Printf("User ID not found in context")
 		http.Error(w, "User ID not found in context", http.StatusUnauthorized)
 		return
 	}
@@ -43,6 +48,7 @@ func (p *PageHandler) BuyerDashboardHandler(w http.ResponseWriter, r *http.Reque
 	// Get property details
 	property, err := p.kenarService.GetPropertyDetail(r.Context(), postToken)
 	if err != nil {
+		log.Printf("Failed to fetch property details: %v", err)
 		http.Error(w, "Failed to fetch property details", http.StatusInternalServerError)
 		return
 	}
@@ -56,19 +62,22 @@ func (p *PageHandler) BuyerDashboardHandler(w http.ResponseWriter, r *http.Reque
 		"PropertyData": property,
 		"IsPurchased":  hasPurchased,
 	}
+
 	tmp, err := template.ParseFiles("./web/buyer_landing.html")
 	if err != nil {
+		log.Printf("Template error: %v", err)
 		http.Error(w, "Template error", http.StatusInternalServerError)
 		return
 	}
 	tmp.ExecuteTemplate(w, "buyer_landing.html", data)
-	return
 }
+
 func (p *PageHandler) SellerDashboardHandler(w http.ResponseWriter, r *http.Request) {
 	postToken := r.URL.Query().Get("post_token")
 	return_url := r.URL.Query().Get("return_url")
 
 	if postToken == "" || return_url == "" {
+		log.Printf("post_token and return_url are required")
 		http.Error(w, "post_token and return_url are required", http.StatusBadRequest)
 		return
 	}
@@ -85,6 +94,7 @@ func (p *PageHandler) SellerDashboardHandler(w http.ResponseWriter, r *http.Requ
 
 	tmp, err := template.ParseFiles("./web/landing.html")
 	if err != nil {
+		log.Printf("Template error: %v", err)
 		http.Error(w, "Template error", http.StatusInternalServerError)
 		return
 	}
@@ -93,8 +103,8 @@ func (p *PageHandler) SellerDashboardHandler(w http.ResponseWriter, r *http.Requ
 		"RedirectLink": return_url,
 		"PropertyData": property,
 	}
+	// TODOOOO
 	tmp.ExecuteTemplate(w, "landing.html", data)
-	return
 }
 
 func (p *PageHandler) AmenitiesPageHandler(w http.ResponseWriter, r *http.Request) {
